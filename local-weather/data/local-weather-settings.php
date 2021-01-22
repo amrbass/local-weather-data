9fe991a22c1f7df93f9c69523d7d1b18d7f7c973
amrbass <amrbass@gmail.com>
2021-01-22 08:55:52 +0100

    added 'data' folder and files with required data and constants

---
+++ local-weather/data/local-weather-settings.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Settings
+ *
+ * Returns an array of valid settings for OpenWeatherMap, with keys being the used 'settings names'.
+ * Values can be used as default values.
+ *
+ * @link       https://ancdretaeixample.cat//sw/info/local-weather/index.php
+ * @since      1.0.0
+ *
+ * @package    Local_Weather
+ * @subpackage Local_Weather/data
+ */
+
+$lwd_settings = [
+	'local_weather_country'	=>	'ad',
+	'local_weather_zipcode'	=>	'AD500',
+	'local_weather_units'	=>	'metric',
+	'local_weather_apikey'	=>	'samplApiKeyGetYourOwn12345678901',
+];
---
+++ local-weather/data/local-weather-units.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Units
+ *
+ * Returns an array of valid 'units' names for OpenWeatherMap.
+ *
+ * @link       https://ancdretaeixample.cat//sw/info/local-weather/index.php
+ * @since      1.0.0
+ *
+ * @package    Local_Weather
+ * @subpackage Local_Weather/data
+ */
+
+$lwd_units = ['standard','metric','imperial'];
