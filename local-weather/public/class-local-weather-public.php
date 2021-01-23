1aa21d1c7d5ff3015b90f6002598f7e82905ea9d
amrbass <amrbass@gmail.com>
2021-01-23 11:04:08 +0100

    real data from OWM when rendering the shortcode

--- local-weather/public/class-local-weather-public.php
+++ local-weather/public/class-local-weather-public.php
@@ -135,15 +135,18 @@ class Local_Weather_Public {
 	 */
 	public function lwd_local_weather_handler( $atts = array(), $content = null, $tag = '' ) {
 
-		$units = ['standard','metric','imperial'];	//OWM API units allowed
+		//$units = ['standard','metric','imperial'];	//OWM API units allowed
+		// get values from settings as default ones
+		$country = get_option($this->lwd_keys[LWD_COUNTRY]);
+		$zipcode = get_option($this->lwd_keys[LWD_ZIPCODE]);
+		$units = get_option($this->lwd_keys[LWD_UNITS]);
 
-		$o = "Local Weather plugin";
-		if($content == "")	{	//only for self-closing shortcode
-			$title = '<h2>Local Weather data</h2>';
-		}	else	{
+		if($content != "")	{	//if it's not a self-closing shortcode
 			$title = $content;
+			$o = '<h2 class="lwd_title">'.$title.'</h2>';
+		}	else{
+			$o = '';
 		}
-		$o = '<h2 class="lwd_title">'.$title.'</h2>';
 
 		// normalize attribute keys, lowercase
 		$atts = array_change_key_case( (array) $atts, CASE_LOWER );
@@ -151,21 +154,123 @@ class Local_Weather_Public {
 		// override default attributes with user attributes
 		$lwd_atts = shortcode_atts(
 			array(
-				'country' => 'es',
-				'zipcode' => '08009',
-				'units' => $units[1],
+				'country' => $country,
+				'zipcode' => $zipcode,
+				'units' => $units,
 			), $atts, $tag
 		);
+		
+		if(array_key_exists(strtolower($lwd_atts['country']), $this->lwd_countries))	{
+			$country_name = $this->lwd_countries[strtolower($lwd_atts['country'])];
+		} else {
+			$country_name = 'bad country code';
+		}
 
-		$o .= '<table class="lwd_table" style="width:60%;">';
-		$o .= '<caption>API Key used: '.get_option($this->lwd_keys[LWD_APIKEY]).'</caption>';
-		$o .= '<tr><td class="lwd_cell">Country:</td><td class="lwd_cell">'.$lwd_atts['country'].' ('.$this->lwd_countries[strtolower($lwd_atts['country'])].')</td></tr>';
-		$o .= '<tr><td class="lwd_cell">ZIP Code:</td><td class="lwd_cell">'.$lwd_atts['zipcode'].'</td></tr>';
-		$o .= '<tr><td class="lwd_cell">Units:</td><td class="lwd_cell">'.$lwd_atts['units'].'</td></tr>';
-		$o .= '<tr><td class="lwd_cell">Language:</td><td class="lwd_cell">'.get_locale().'</td></tr>';
-		$o .= '</table>';
+		//access OpenWeatherMap data
+		$apiKey = get_option($this->lwd_keys[LWD_APIKEY]);
+		$lang = get_locale();
+		$OWMurl = "http://api.openweathermap.org/data/2.5/weather";
+		$googleApiUrl = $OWMurl."?zip=".$lwd_atts['zipcode'].",".$lwd_atts['country']."&lang=".$lang."&units=".$lwd_atts['units']."&APPID=$apiKey";
+		$data = $this->lwd_getWeather($googleApiUrl);
+
+		//handle { "cod": "404", "message": "city not found" } or similar
+		if ($data->cod > 400) {	//error
+			$o .= '<h4 style="color:red;">'.$data->message.' for ZIP code '.$lwd_atts["zipcode"].' in '.$country_name.'</h4>';
+		}	else	{
+			
+			//setlocale(LC_TIME,"ca_ES");
+			setlocale(LC_TIME, "ca_ES");
+			$currentTime = strftime("%c", $data->dt);
+
+			$o .= '<table class="lwd_table" style="width:100%;">';
+			$o .= '<caption>Weather now in '.$data->name.' ('.$country_name.')<br />'.$currentTime.'</caption>';
+			$o .= '<tr><td class="lwd_cell2"><img src="http://openweathermap.org/img/w/'.$data->weather[0]->icon.'.png"/></td><td class="lwd_cell">'.ucwords($data->weather[0]->description).'</td><td class="lwd_cell"></td></tr>';
+			$o .= '<tr><td class="lwd_cell">Temperature: '.round($data->main->temp).' ºC</td><td class="lwd_cell">(feels like '.round($data->main->feels_like).' ªC)</td></tr>';
+			$o .= '<tr><td class="lwd_cell">Humidity: '.round($data->main->humidity).' %</td><td class="lwd_cell"></td></tr>';
+			$o .= '<tr><td class="lwd_cell">Pressure: '.round($data->main->pressure).' hPa</td><td class="lwd_cell"></td></tr>';
+			$o .= '</table>';
+
+			//only for debur purpose. Comment this line when done
+			$o .= '<br /><div style="color:white;padding:10px;background-color:black;overflow:hidden;"><pre><code>'.json_encode($data, JSON_PRETTY_PRINT).'</code></pre></div>';
+
+		}
 
 		return $o;
+	}
+
+	/**
+	 * cURL request to get OWM data.
+	 *
+	 * @since	1.0.0
+	 * @access   private
+	 * @param	string	$url	The complete url to access.
+	 * @return	string	An object with all data provided by OWM.
+	 */
+	private function lwd_getWeather($url)	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($ch, CURLOPT_VERBOSE, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$response = curl_exec($ch);
+		curl_close($ch);
+		return json_decode($response);
+	}
+
+	/**
+	 * Degrees to win directoin converter.
+	 *
+	 * @since	1.0.0
+	 * @access   private
+	 * @param	int		$deg	Degrees value.
+	 * @return	string	Wind direction standard name.
+	 */
+  	private function lwd_degrees_to_windir($deg) {
+		$directions = array('N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N');
+		return $directions[(round($deg / 22.5) % 16)];
+	}
+
+	/**
+	 * Wind speed to Beaufort scale conversion.
+	 *
+	 * @since	1.0.0
+	 * @access	private
+	 * @param	float	$gust	Wind speed in m/s.
+	 * @return	string	Beaufort wind type name.
+	 */
+	private function lwd_ms_to_Beaufort($gust) {
+		$desc = '';
+		if($gust < 0.3)
+			$desc = 'Calma';
+		elseif($gust < 1.6)
+			$desc = 'Ventolina';
+		elseif ($gust < 3.4)
+			$desc = 'Vent fluixet';
+		elseif ($gust < 5.5)
+			$desc = 'Vent fluix';
+		elseif ($gust < 8.0)
+			$desc = 'Vent moderat';
+		elseif ($gust < 10.8)
+			$desc = 'Vent fresquet';
+		elseif ($gust < 13.9)
+			$desc = 'Vent fresc';
+		elseif ($gust < 17.2)
+			$desc = 'Vent fort';
+		elseif ($gust < 20.8)
+			$desc = 'Temporal';
+		elseif ($gust < 24.5)
+			$desc = 'Temporal fort';
+		elseif ($gust < 28.5)
+			$desc = 'Temporal molt fort';
+		elseif ($gust < 32.7)
+			$desc = 'Temporal violent';
+		elseif ($gust >= 32.7)
+			$desc = 'Huracà';
+		else
+			$desc = 'valor desconegut';
+		return("$desc");
 	}
 
 }
